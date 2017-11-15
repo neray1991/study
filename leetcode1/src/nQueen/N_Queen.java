58402e07898889e194c208b3903c03a970fa65a2
root <root@neray-OptiPlex-3020.(none)>
2017-11-14 17:53:12 -0800

    NQueens2

--- leetcode1/src/nQueen/N_Queen.java
+++ leetcode1/src/nQueen/N_Queen.java
@@ -36,7 +36,7 @@ public class N_Queen {
 	}
 	
 	public void solveNQueensRe(int n, int row, int ld, int rd, List<char[]> sol, List<String[]> res) {
-		if (row == (1 << n) - 1) {
+		if (row == (1 << n) - 1) {//Only here we find a solution.
 			String[] temp = new String[n];
 			for (int i = 0; i < n; i++)
 				temp[i] = String.valueOf(sol.get(i));
@@ -51,18 +51,97 @@ public class N_Queen {
 				Arrays.fill(str, '.');
 				str[i] = 'Q';
 				sol.add(str);
-				solveNQueensRe(n, row | pos, (ld | pos) << 1, (rd | pos) >> 1, sol, res);
+				solveNQueensRe(n, row | pos, (ld | pos) << 1, (rd | pos) >> 1, sol, res); //Row can also be column here.
 				sol.remove(sol.size() - 1);
 				
 			}
 		}
 	}
+	
+	public int totalNQueens_1(int n) {
+		int[] board = new int[n];
+		Arrays.fill(board, -1);
+		int[] res = new int[1];
+		totalNQueensRe(n, 0, board, res);
+		return res[0];
+	}
+	
+	public void totalNQueensRe(int n, int row, int[] board, int[] res) {
+		if (n == row) {
+			res[0]++;
+			return;
+		}
+		for (int i = 0; i < n; i++) {
+			if (isValid(board, row, i)) {
+				board[row] = i;
+				totalNQueensRe(n, row + 1, board, res);
+				board[row] = -1;
+			}
+		}
+	}
+	
+	public boolean isValid(int[] board, int row, int col) {
+		for (int i = 0; i < row; i++) {
+			if (board[i] == col || row - i == Math.abs(col - board[i])) //row - row = +-(col - col) means they are on the same diagonal.
+				return false;
+		}
+		return true;
+	}
+	
+	public int totalNQueens_2(int n) {
+		int[] res = new int[1];
+		totalNQueensRe2(n, 0, 0, 0, res);
+		return res[0];
+	}
+	
+	public void totalNQueensRe2(int n, int row, int ld, int rd, int[] res) {
+		if (row == (1<<n) - 1) {
+			res[0]++;
+			return;
+		}
+		int avail = ~(row | ld | rd);
+		for (int i = n - 1; i >= 0; i--) {
+			int pos = 1 << i;
+			if ((int)(pos & avail) != 0) {
+				totalNQueensRe2(n, row | pos, (ld | pos) << 1, (rd | pos) >> 1, res);
+			}
+		}
+	}
+	
+	public int totalNQueens_3(int n) {
+		int[] a = new int[n];
+		Arrays.fill(a, -1);
+		int res = 0;
+		int row = 0;
+		while (row >= 0) {
+			if (row == n) {
+				res++; row--;
+			}
+			int i = a[row] == -1 ? 0 : a[row] + 1;
+			for (; i < n; i++) {
+				if (isValid(a, row, i)) {
+					a[row] = i;
+					row++;
+					break;
+				}
+			}
+			if (i == n) {
+				a[row] = -1;
+				row--;
+			}
+		}
+		return res;
+	}
+	
 	public static void main(String args[]) {
-		List<String[]> res = new N_Queen().solveNQueens(8);
+		List<String[]> res = new N_Queen().solveNQueens(5);
 		for (int i = 0; i < res.size(); i++) {
 			for (int j = 0; j < res.get(i).length; j++)
 				System.out.println(res.get(i)[j]);
 			System.out.println("**********");
 		}
+		System.out.println(new N_Queen().totalNQueens_1(5));
+		System.out.println(new N_Queen().totalNQueens_2(5));
+		System.out.println(new N_Queen().totalNQueens_3(5));
 	}
 }
